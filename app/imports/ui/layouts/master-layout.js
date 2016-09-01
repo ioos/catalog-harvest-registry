06e7ad17a55cf239c458b051012e5b5bbbde2edb
Luke Campbell <luke.s.campbell@gmail.com>
2016-09-01 14:07:41 -0400

    Hopefully fixes 'Processing' bug

--- app/imports/ui/layouts/master-layout.js
+++ app/imports/ui/layouts/master-layout.js
@@ -5,3 +5,6 @@ import './master-layout.less';
 
 import { Template } from 'meteor/templating';
 
+Template.MasterLayout.onCreated(function () {
+    window.HTML.isConstructedObject = function(x) { return _.isObject(x) && !$.isPlainObject(x); };
+});
