ce046f117e174cf32167901ede32055a16ca6473
Luke Campbell <luke.s.campbell@gmail.com>
2017-03-27 11:42:39 -0400

    Start to implement authenticated API

--- app/imports/api/harvests/api.js
+++ app/imports/api/harvests/api.js
@@ -24,6 +24,32 @@ if (Meteor.isServer) {
     }
   });
 
+  let ApiV2 = new Restivus({
+    version: 'v2',
+    useDefaultAuth: true,
+    prettyJson: (Meteor.isDevelopment) ? true : false
+  });
+
+  ApiV2.addRoute('harvests/:id/harvest', {
+    authRequired: true
+  },{
+    get() {
+      console.log(this.urlParams.id);
+      return {"test":"success"};
+    }
+  });
+  /*
+   * To Authenticate
+   * curl -i -d 'email=lcampbell@ioos.us&password=da7655b5bf67039c3e76a99d8e6fb6969370bbc0fa440cae699cf1a3e2f1e0a1&hashed=true' -XPOST localhost:3000/api/v2/login
+   */
+
+  /*
+   * To execute:
+   * curl -H "X-Auth-Token: 11RNttydywkBJj5Qwj-PiREawMRr0wQkmPvu6fHPbVF" -H "X-User-Id: urgkbrjoussWi55M2" -i localhost:3000/api/v2/harvests/abc123/harvest
+   */
+
+
+
 }
 
 // Representational State (REST) endpoints disabled by default.
