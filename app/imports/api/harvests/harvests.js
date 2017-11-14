b8269875ede9e19770a6f5a01f37dbff9acd24a5
Eric Bernier <ericmbernier@gmail.com>
2017-11-14 16:16:43 -0500

    CHR-108 - Defaulted contact to user email

--- app/imports/api/harvests/harvests.js
+++ app/imports/api/harvests/harvests.js
@@ -82,93 +82,119 @@ export const HarvestsTable = new Tabular.Table({
   }
 });
 
-const HarvestSchema = new SimpleSchema({
-  _id: {
-    type: String
-  },
-  name: {
-    type: String
-  },
-  url: {
-    type: String,
-    unique: true,
-    label: "URL"
-  },
-  organization: {
-    type: String,
-    label: "Organization"
-  },
-  /* harvest interval in seconds.  Initialize to null to indicate no harvest
-   * made */
-  last_harvest_dt: {
-    type: null,
-    optional: true
-  },
-  harvest_interval: {
-    type: Number,
-    optional: true,
-    defaultValue: 1
-  },
-  harvest_type: {
-    type: String,
-    allowedValues: ['WAF', 'ERDDAP-WAF', 'CSW'],
-    defaultValue: 'WAF'
-  },
-  publish: {
-    type: Boolean,
-    defaultValue: false,
-    label: "Publish this source?"
-  },
-  harvest_contact: {
-    type: String,
-    optional: true,
-    label: "Contact"
-  }
-});
-Harvests.schema = HarvestSchema;
-
-// add public Fields
-Harvests.publicFields = {
-  _id: 1,
-  name: 1,
-  url: 1,
-  organization: 1,
-  last_harvest_dt: 1,
-  harvest_interval: 1,
-  harvest_type: 1,
-  publish: 1,
-  harvest_contact: 1
-};
-
-Harvests.attachSchema(HarvestSchema);
-
-
+var schemaLoaded = false;
 if (Meteor.isClient) {
-  Harvests.allow({
-    insert(userId, doc) {
-      return false;
-    },
+  Tracker.autorun(function(){  
+    if (schemaLoaded) {
+      return;
+    }
 
-    update(userId, doc, fieldNames, modifier) {
-      return false;
-    },
-
-    remove(userId, doc) {
-      return false;
+    var harvestuid = Meteor.userId();
+    var harvestUsr = Meteor.users.findOne({ _id: harvestuid });
+    
+    if (harvestUsr) {      
+      harvestUserEmail = harvestUsr.emails[0].address;
+      loadHarvestSchema();
+      schemaLoaded = true;      
     }
   });
+}
 
-  Harvests.deny({
-    insert(userId, doc) {
-      return true;
+if (Meteor.isServer && !schemaLoaded) {
+  var curHarvestUsr = Meteor.users.findOne({ _id: Meteor.userId });  
+  harvestUserEmail = curHarvestUsr.emails[0].address;
+  loadHarvestSchema();
+}
+
+function loadHarvestSchema() {
+  const HarvestSchema = new SimpleSchema({
+    _id: {
+      type: String
     },
-
-    update(userId, doc, fieldNames, modifier) {
-      return true;
+    name: {
+      type: String
     },
-
-    remove(userId, doc) {
-      return true;
+    url: {
+      type: String,
+      unique: true,
+      label: "URL"
+    },
+    organization: {
+      type: String,
+      label: "Organization"
+    },
+    /* harvest interval in seconds.  Initialize to null to indicate no harvest
+    * made */
+    last_harvest_dt: {
+      type: null,
+      optional: true
+    },
+    harvest_interval: {
+      type: Number,
+      optional: true,
+      defaultValue: 1
+    },
+    harvest_type: {
+      type: String,
+      allowedValues: ['WAF', 'ERDDAP-WAF', 'CSW'],
+      defaultValue: 'WAF' 
+    },
+    publish: {
+      type: Boolean,
+      defaultValue: false,
+      label: "Publish this source?"
+    },
+    harvest_contact: {
+      type: String,
+      defaultValue: harvestUserEmail,
+      optional: true,
+      label: "Contact"
     }
   });
+  Harvests.schema = HarvestSchema;     
+
+  // add public Fields
+  Harvests.publicFields = {
+    _id: 1,
+    name: 1,
+    url: 1,
+    organization: 1,
+    last_harvest_dt: 1,
+    harvest_interval: 1,
+    harvest_type: 1,
+    publish: 1,
+    harvest_contact: 1
+  };
+
+  Harvests.attachSchema(HarvestSchema);
+
+  if (Meteor.isClient) {
+    Harvests.allow({
+      insert(userId, doc) {
+        return false;
+      },
+
+      update(userId, doc, fieldNames, modifier) {
+        return false;
+      },
+
+      remove(userId, doc) {
+        return false;
+      }
+    });
+
+    Harvests.deny({
+      insert(userId, doc) {
+        return true;
+      },
+
+      update(userId, doc, fieldNames, modifier) {
+        return true;
+      },
+
+      remove(userId, doc) {
+        return true;
+      }
+    });
+  }
 }
